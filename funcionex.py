61b1b94329c4d751515d59c02dfdfda1111d3530
tenkomati <matias.szwn@gmail.com>
2023-07-12 01:35:19 -0300

    hello

--- funcionex.py
+++ funcionex.py
@@ -5,13 +5,8 @@ def softmax(*args):
   #is rather a smooth approximation to the arg max function: the function whose value is which index has the maximum.
 
   inputs=list(args) # a list of the args
-  exp_inputs = [] #a vector of the inputs as an exponent of exp() function
-  outputs=[] #the output of softmax
-
-  for i in inputs:
-    exp_inputs.append(math.exp(i))
-  for i in exp_inputs:
-    outputs.append(i / sum(exp_inputs))
+  exp_inputs = [math.exp(i) for i in inputs] #a vector of the inputs as an exponent of exp() function
+  outputs=[i / sum(exp_inputs) for i in exp_inputs] #the output of softmax
 
   return outputs
 
@@ -22,29 +17,10 @@ def argmax(*args):
   # if there are more than one max value, the function will divide each one by the number of max values.
 
   inputs=list(args) # a list of the args
-  exp_inputs = [] #a vector of the inputs as an exponent of exp() function
-  outputs=[] #the output of softmax
-  hot_outputs = [] #the one hot vector of the max of the output
-
-  for i in inputs:
-    exp_inputs.append(math.exp(i))
-  for i in exp_inputs:
-    outputs.append(i / sum(exp_inputs))
-
-  max_num= max(outputs) #the max value of the outputs
-
-  n_max = int()
-
-  for i in outputs:
-    if i == max_num:
-      n_max += 1
-
+  exp_inputs = [math.exp(i) for i in inputs] #a vector of the inputs as an exponent of exp() function
+  outputs=[i / sum(exp_inputs) for i in exp_inputs] #the output of softmax
+  max_num= max(outputs) 
+  n_max = sum(1 for i in outputs if i == max_num)
+  hot_outputs = [1/n_max if i == max_num else 0 for i in outputs] #the one hot vector of the max of the output
   
-
-  for i in outputs:
-    if i == max_num:
-      hot_outputs.append(1/n_max)
-    else:
-      hot_outputs.append(0)
-
   return hot_outputs
